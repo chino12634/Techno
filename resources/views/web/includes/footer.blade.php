dc452eb412cf28837c6278c2ad3e9e94fb503851
Kent <kentharveylasmarias393@gmail.com>
2024-03-24 07:39:01 +0800

    ..

--- resources/views/web/includes/footer.blade.php
+++ resources/views/web/includes/footer.blade.php
@@ -12,9 +12,7 @@
                         <ul>
                             <li><a href="#!"><i class="fab fa-facebook-f"></i></a></li>
                             <li><a href="#!"><i class="fab fa-twitter"></i></a></li>
-                            <li><a href="#!"><i class="fab fa-linkedin"></i></a></li>
                             <li><a href="#!"><i class="fab fa-instagram"></i></a></li>
-                            <li><a href="#!"><i class="fab fa-google-plus-g"></i></a></li>
                         </ul>
                     </div>
                 </div>
